48e81fa09ecbaad2a72db3989bb4aff3830354d1
asusoft <ashehu19a@gmail.com>
2023-04-16 19:52:29 +0300

    constants

---
+++ HifzMate/assets/constants/colors.js
@@ -0,0 +1,11 @@
+const COLORS = {
+    primary: '#994EF8',
+    secondary: '#F672CBC',
+    background: '#FAF0E6',
+    active_icon: "#F3BF82",
+    inactive: "#E8E9EE",
+    white: "#FFFFFF",
+    icons: "#8789A3"
+  };
+  
+  export default COLORS;
---
+++ HifzMate/assets/constants/icons.js
@@ -0,0 +1,17 @@
+const ayah = require('../images/ayah.png');
+const back = require('../images/back.png');
+const cancel = require('../images/cancel.png');
+const challenge = require('../images/challenge.png');
+const menu = require('../images/menu.png');
+const quranSmall = require('../images/quran-small.png');
+const reader = require('../images/reader.png');
+
+export default {
+    ayah,
+    back,
+    cancel,
+    challenge,
+    menu,
+    quranSmall,
+    reader
+}   
